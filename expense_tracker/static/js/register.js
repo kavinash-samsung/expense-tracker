6f6e50f245b91c9d4c519da64998dcdee6cb430e
avi1501 <officialaviid@gmail.com>
2022-02-06 04:29:45 +0530

    fields required

--- expense_tracker/static/js/register.js
+++ expense_tracker/static/js/register.js
@@ -4,12 +4,12 @@ const usernameSuccessOutput = document.querySelector(".usernameSuccessOutput");
 
 usernameField.addEventListener("keyup", (e)=>{
     const usernameVal = e.target.value;
-    usernameSuccessOutput.style.display="block";
-    usernameSuccessOutput.textContent = `Checking ${usernameVal}`;
-   
+    
     usernameField.classList.remove("is-invalid");
     invalidUsernameField.style.display="none";
     if(usernameVal.length>0){
+        usernameSuccessOutput.style.display="block";
+        usernameSuccessOutput.textContent = `Checking ${usernameVal}`;
         fetch("/authentication/validate-username/", {
             body:JSON.stringify({username:usernameVal}),
             method:"POST",
@@ -31,12 +31,12 @@ const emailSuccessOutput = document.querySelector(".emailSuccessOutput");
 
 emailField.addEventListener("keyup", (e)=>{
     const emailVal = e.target.value;
-    emailSuccessOutput.style.display = "block";
-    emailSuccessOutput.textContent = `Checking ${emailVal}`
     
     emailField.classList.remove("is-invalid");
     invalidEmailField.style.display="none";
     if(emailVal.length>0){
+        emailSuccessOutput.style.display = "block";
+        emailSuccessOutput.textContent = `Checking ${emailVal}`
         fetch("/authentication/validate-email/", {
             body:JSON.stringify({email:emailVal}),
             method:"POST",
