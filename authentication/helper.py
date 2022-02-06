5edf55293477d4ef54cd65c206f52ea37e85db7a
avi1501 <officialaviid@gmail.com>
2022-02-07 00:55:44 +0530

    added gitignore

--- authentication/helper.py
+++ authentication/helper.py
@@ -2,12 +2,12 @@ from email.message import EmailMessage
 
 def send_email_to_newly_registered_user(user_email):
     email_subject = "Activate Your Account"
-    email_body = ""
+    email_body = "Test body"
     email = EmailMessage(
         email_subject,
         email_body,
         "noreply@semicolon.com",
         [user_email],
-
-
     )
+    email.send(fail_sendly=False)
+    
